fd7bdf61d30882d996dd1fa6e8db1337f093d02c
K.Miszel <k.miszel@protonmail.com>
2024-10-28 22:40:50 +0100

    wrap up

--- providers.tf
+++ providers.tf
@@ -12,8 +12,7 @@ terraform {
     container_name       = "terraform"
     key                  = "tfstate"
     use_oidc             = true
-    subscription_id      = "2745b794-365d-4993-9eec-fe3f9879434b"
-    tenant_id            = "48c383d8-47c5-48f9-9e8b-afe4f2519054"
+    subscription_id      = var.hub_state_sub_id
   }
 }
 
@@ -32,5 +31,5 @@ provider "azurerm" {
   alias = "hub"
   features {}
   use_oidc        = true
-  subscription_id = "1c7e8402-ac8b-48d9-a8c8-f3de95064482"
+  subscription_id = var.hub_subscription_id
 }
