7c4f1ac0f6e509cd1a48dae021c0016dfbb8188f
K.Miszel <k.miszel@protonmail.com>
2024-10-28 21:21:33 +0100

    peering

--- data.tf
+++ data.tf
@@ -1,11 +1,11 @@
-# data "terraform_remote_state" "hub-vnet_tfstate" {
-#   backend = "azurerm"
+data "terraform_remote_state" "hub-vnet_tfstate" {
+  backend = "azurerm"
 
-#   config = {
-#     storage_account_name = var.state_sa_name
-#     container_name       = var.container_name
-#     subscription_id      = var.hub_sub_id
-#     resource_group_name  = var.hub_rg_name
-#     key                  = var.key
-#   }
-# }
+  config = {
+    storage_account_name = var.state_sa_name
+    container_name       = var.container_name
+    subscription_id      = var.hub_sub_id
+    resource_group_name  = var.hub_rg_name
+    key                  = var.key
+  }
+}
--- main.tf
+++ main.tf
@@ -7,26 +7,26 @@ module "spoke_networking" {
   subnets         = var.subnets
 }
 
-# module "peering" {
-#   source = "./modules/peering"
-#   providers = {
-#     azurerm.spoke = azurerm.spoke
-#     azurerm.hub   = azurerm.hub
-#   }
-#   spoke_rg_name                = module.spoke_networking.spoke_rg_name
-#   spoke_vnet_name              = module.spoke_networking.spoke_vnet_name
-#   spoke_vnet_id                = module.spoke_networking.spoke_vnet_id
-#   hub_rg_name                  = data.terraform_remote_state.hub-vnet_tfstate.outputs.hub_rg_name
-#   hub_vnet_name                = data.terraform_remote_state.hub-vnet_tfstate.outputs.hub_vnet_name
-#   hub_vnet_id                  = data.terraform_remote_state.hub-vnet_tfstate.outputs.hub_vnet_id
-#   allow_virtual_network_access = true
-#   allow_forwarded_traffic      = true
-#   allow_gateway_transit        = false
-#   use_remote_gateways          = false
-#   depends_on = [
-#     module.spoke_networking
-#   ]
-# }
+module "peering" {
+  source = "./modules/peering"
+  providers = {
+    azurerm.spoke = azurerm.spoke
+    azurerm.hub   = azurerm.hub
+  }
+  spoke_rg_name                = module.spoke_networking.spoke_rg_name
+  spoke_vnet_name              = module.spoke_networking.spoke_vnet_name
+  spoke_vnet_id                = module.spoke_networking.spoke_vnet_id
+  hub_rg_name                  = data.terraform_remote_state.hub-vnet_tfstate.outputs.hub_rg_name
+  hub_vnet_name                = data.terraform_remote_state.hub-vnet_tfstate.outputs.hub_vnet_name
+  hub_vnet_id                  = data.terraform_remote_state.hub-vnet_tfstate.outputs.hub_vnet_id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+  depends_on = [
+    module.spoke_networking
+  ]
+}
 
 # module "acr_dns_zone" {
 #   source              = "./modules/dns_zone"
