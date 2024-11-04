5bebe373d0a42145a384c59c06c0e7d6bd23b970
K.Miszel <k.miszel@protonmail.com>
2024-11-04 18:39:10 +0100

    rem_all

--- locals.tf
+++ locals.tf
@@ -1,29 +1,29 @@
-locals {
-  appgws = {
-    "appgw_blue" = {
-      name_prefix   = "blue"
-      appgw_turn_on = true
-    },
-    "appgw_green" = {
-      name_prefix   = "green"
-      appgw_turn_on = false
-    }
-  }
-}
+# locals {
+#   appgws = {
+#     "appgw_blue" = {
+#       name_prefix   = "blue"
+#       appgw_turn_on = true
+#     },
+#     "appgw_green" = {
+#       name_prefix   = "green"
+#       appgw_turn_on = false
+#     }
+#   }
+# }
 
-locals {
-  aks_clusters = {
-    "aks_blue" = {
-      name_prefix = "blue"
-      aks_turn_on = true
-      k8s_version = "1.29"
-      appgw_name  = "blue-appgw"
-    },
-    "aks_green" = {
-      name_prefix = "green"
-      aks_turn_on = false
-      k8s_version = "1.29"
-      appgw_name  = "green-appgw"
-    }
-  }
-}
+# locals {
+#   aks_clusters = {
+#     "aks_blue" = {
+#       name_prefix = "blue"
+#       aks_turn_on = true
+#       k8s_version = "1.29"
+#       appgw_name  = "blue-appgw"
+#     },
+#     "aks_green" = {
+#       name_prefix = "green"
+#       aks_turn_on = false
+#       k8s_version = "1.29"
+#       appgw_name  = "green-appgw"
+#     }
+#   }
+# }
