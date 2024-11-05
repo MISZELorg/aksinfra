a8978ecaec70c30a9c3c615b0d57f405c78eaf13
K.Miszel <k.miszel@protonmail.com>
2024-11-05 10:46:01 +0100

    add_spoke_aks_monitoring

--- modules/EID_role_assignment/main.tf
+++ modules/EID_role_assignment/main.tf
@@ -1,11 +1,14 @@
+# resource "azurerm_role_assignment" "eid_group_assignment" {
+#   for_each             = toset(var.eid_group_ids)
+#   scope                = var.scope
+#   role_definition_name = var.role_definition_name
+#   principal_id         = each.value
+# }
+
 resource "azurerm_role_assignment" "eid_group_assignment" {
-  for_each             = toset(var.eid_group_ids)
+  for_each = var.eid_group_ids != [] ? zipmap(var.eid_group_ids, var.eid_group_ids) : {}
+
   scope                = var.scope
   role_definition_name = var.role_definition_name
   principal_id         = each.value
-}
-
-output "role_assignment_ids" {
-  description = "The IDs of the created role assignments for EID groups."
-  value       = [for ra in azurerm_role_assignment.eid_group_assignment : ra.id]
 }
--- modules/EID_role_assignment/outputs.tf
+++ modules/EID_role_assignment/outputs.tf
@@ -2,3 +2,8 @@ output "eid_role_assignment_ids" {
   description = "The IDs of the created role assignments for EID groups."
   value       = [for ra in azurerm_role_assignment.eid_group_assignment : ra.id]
 }
+
+# output "role_assignment_ids" {
+#   description = "The IDs of the created role assignments for EID groups."
+#   value       = [for ra in azurerm_role_assignment.eid_group_assignment : ra.id]
+# }
