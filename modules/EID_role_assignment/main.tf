71cf39b60e2e7d150c7d4246509d6d9b1c75b96c
K.Miszel <k.miszel@protonmail.com>
2024-11-05 10:48:07 +0100

    add_spoke_aks_monit

--- modules/EID_role_assignment/main.tf
+++ modules/EID_role_assignment/main.tf
@@ -6,7 +6,7 @@
 # }
 
 resource "azurerm_role_assignment" "eid_group_assignment" {
-  for_each = var.eid_group_ids != [] ? zipmap(var.eid_group_ids, var.eid_group_ids) : {}
+  for_each = { for idx, id in var.eid_group_ids : idx => id }
 
   scope                = var.scope
   role_definition_name = var.role_definition_name
